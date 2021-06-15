3f7685e7efdf4060ecc9e7f41ece2884f2f7eecd
maheenur13 <differentmahee13@gmail.com>
2021-06-15 10:12:57 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+
+## Project setup
+You can use yarn or npm to run the software locally. I typically use npm. 
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,94 @@
+<template>
+  <div id="app">
+    <h1 class="title"> Calculator </h1>
+    <div class="columns">
+      <div class="column">
+        <div class="field">
+          <div class="control">
+            First Number
+            <input class="input" type="number" placeholder="First Number" v-model.number="first_number" @change="checkNegative" min="0" />
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="field">
+          <div class="control">
+            Second Number
+            <input class="input" type="number" placeholder="Second Number" v-model.number="second_number" @change="checkNegative" min="0" />
+          </div>
+        </div>
+      </div>      
+    </div>
+   
+      <div class="column has-text-left">
+        <p>Addition: {{ addition() }}</p>
+        <p>Subtraction: {{ subtraction() }}</p>
+        <p>Multiplication: {{ multiplication() }}</p>
+        <p>Division: {{ division() }}</p>
+      
+       
+      </div>  
+    </div>
+    
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+      return {
+        first_number: '0',
+        second_number: '0'
+      }
+    },
+    methods: {
+      addition() {
+        var firstNumber = (parseInt(this.first_number));
+       var secondNumber = (parseInt(this.second_number));
+       return  firstNumber+secondNumber;
+       
+        // return Math.round(total * 100) / 100
+      },
+      subtraction() {
+        var firstNumber = (parseInt(this.first_number));
+        var secondNumber = (parseInt(this.second_number));
+        return firstNumber-secondNumber;
+      },
+      multiplication() {
+        var firstNumber = (parseInt(this.first_number));
+        var secondNumber = (parseInt(this.second_number));
+        return firstNumber*secondNumber;
+      },
+      division() {
+        var firstNumber = (parseInt(this.first_number));
+        var secondNumber = (parseInt(this.second_number));
+        return firstNumber/secondNumber;
+      },
+      
+      checkNegative() {
+        if (this.first_number < 0){
+          this.first_number = 0;
+        }
+        if (this.second_number < 0){
+          this.second_number = '0';
+        }
+      }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0px auto;
+  margin-top: 60px;
+  width:50%;
+  padding: 2em;
+  box-shadow: 0 0 5px 5px #666;
+}
+</style>
